61de78654d73c0f0c26bf24d981c72d5003fdded
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-12 09:59:06 +0200

    feat: implement pill UI to use dyanamic mapping

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,39 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
 import { PROPERTYLISTINGSAMPLE } from "@/constants";
 import PropertyCard from "@/components/common/PropertyCard";
+import Pill from "@/components/common/Pill";
+import { HERO_BACKGROUND } from "@/constants";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const leftPills = [
+  "All",
+  "Top Villa",
+  "Free Reschedule",
+  "Book Now, Pay later",
+  "Self Checkin",
+  "Instant Book",
+];
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const rightPills = ["Filter", "Sort by: Higest Price"];
 
 export default function Home() {
   return (
     <>
       <div className="px-4 md:px-8 py-6">
         <h2 className="text-2xl font-bold mb-6">Featured Properties</h2>
+        <div className="flex justify-between mb-6">
+          <div className="flex gap-20">
+            {leftPills.map((label) => (
+              <Pill key={label} label={label} />
+            ))}
+          </div>
+          <div className="flex gap-4">
+            {rightPills.map((label) => (
+              <Pill key={label} label={label} />
+            ))}
+          </div>
+        </div>
         <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3">
           {PROPERTYLISTINGSAMPLE.map((property, index) => (
-            <div>
+            <div className="flex justify-center">
               <PropertyCard
                 key={index}
                 name={property.name}
