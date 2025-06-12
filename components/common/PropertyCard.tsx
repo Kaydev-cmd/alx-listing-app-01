da2b87117f1997b17ec94ccad4b7848039ed118d
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-12 12:11:47 +0200

    feat: display property aminities as pill-style tags

--- components/common/PropertyCard.tsx
+++ components/common/PropertyCard.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { PropertyCardProps } from "@/interfaces";
 
+const amenities = ["Top Villa", "Self Checkin", "Free Reschedule"];
+
 const PropertyCard: React.FC<PropertyCardProps> = ({
   name,
   price,
@@ -10,10 +12,19 @@ const PropertyCard: React.FC<PropertyCardProps> = ({
   return (
     <div className="bg-white rounded-xl shadow-md overflow-hidden w-full max-w-sm transition-transform hover:scale-105 cursor-pointer">
       <img src={image} alt={name} className="w-full h-48 object-cover" />
+      <ul className="flex flex-wrap space-x-4 mt-2 ml-2">
+        {amenities.map((amenity, index) => (
+          <li key={index} className="bg-gray-200 rounded-md py-1 px-2 text-sm font-semibold">
+            {amenity}
+          </li>
+        ))}
+      </ul>
       <div className="p-4">
         <h3 className="text-lg font-semibold mb-1">{name}</h3>
+        <div className="flex justify-between">
         <p className="text-[#34967C] font-medium mb-1">{price}</p>
         <p className="text-sm text-gray-500">{rating}</p>
+        </div>
       </div>
     </div>
   );
