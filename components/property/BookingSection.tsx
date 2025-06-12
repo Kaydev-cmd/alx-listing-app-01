91f11c5389d1b5cd528d73f7256ebc880d810f8b
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-12 10:46:45 +0200

    feat: add check-out date input to BookingSSection component

--- components/property/BookingSection.tsx
+++ components/property/BookingSection.tsx
@@ -13,6 +13,15 @@ const BookingSection: React.FC<{ price: number }> = ({ price }) => {
           id="date"
         />
       </div>
+      <div className="mt-4">
+        <label htmlFor="date">Check-out</label>
+        <input
+          type="date"
+          className="border p-2 w-full mt-2"
+          name="date"
+          id="date"
+        />
+      </div>
     </div>
   );
 };
