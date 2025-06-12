59049b9c1a6de84d3c7741cb904431c80d23a2e9
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-12 16:54:38 +0200

    refactor: refactor Carousel component to include alt text for improved accessibility

--- components/common/Carousel.tsx
+++ components/common/Carousel.tsx
@@ -2,31 +2,90 @@ import { CarouselProps } from "@/interfaces";
 import React from "react";
 
 const logos = [
-  "/assets/logos/apartment1.png",
-  "/assets/logos/barn1.png",
-  "/assets/logos/cabin1.png",
-  "/assets/logos/castle-tower1.png",
-  "/assets/logos/cottage1.png",
-  "/assets/logos/farm1.png",
-  "/assets/logos/farm1(1).png",
-  "/assets/logos/home1.png",
-  "/assets/logos/island(1)1.png",
-  "/assets/logos/key-chain1.png",
-  "/assets/logos/living-room1.png",
-  "/assets/logos/mansion1.png",
-  "/assets/logos/palm-tree1.png",
-  "/assets/logos/swimming-pool1.png",
-  "/assets/logos/tent1.png",
-  "/assets/logos/treehouse(1)1.png",
-  "/assets/logos/vacations1.png",
-  "/assets/logos/villa1.png",
+  {
+    image: "/assets/logos/apartment1.png",
+    alt: "Apartment",
+  },
+  {
+    image: "/assets/logos/barn1.png",
+    alt: "Barn",
+  },
+  {
+    image: "/assets/logos/cabin1.png",
+    alt: "Cabin",
+  },
+  {
+    image: "/assets/logos/castle-tower1.png",
+    alt: "Castle Tower",
+  },
+  {
+    image: "/assets/logos/cottage1.png",
+    alt: "Cottage",
+  },
+  {
+    image: "/assets/logos/farm1.png",
+    alt: "Farm",
+  },
+  {
+    image: "/assets/logos/farm1(1).png",
+    alt: "Farm",
+  },
+  {
+    image: "/assets/logos/home1.png",
+    alt: "Home",
+  },
+  {
+    image: "/assets/logos/island(1)1.png",
+    alt: "Island",
+  },
+  {
+    image: "/assets/logos/key-chain1.png",
+    alt: "Key chain",
+  },
+  {
+    image: "/assets/logos/living-room1.png",
+    alt: "Living room",
+  },
+  {
+    image: "/assets/logos/mansion1.png",
+    alt: "Mansion",
+  },
+  {
+    image: "/assets/logos/palm-tree1.png",
+    alt: "Palm tree",
+  },
+  {
+    image: "/assets/logos/swimming-pool1.png",
+    alt: "Swimming pool",
+  },
+  {
+    image: "/assets/logos/tent1.png",
+    alt: "Tent",
+  },
+  {
+    image: "/assets/logos/treehouse(1)1.png",
+    alt: "Treehouse",
+  },
+  {
+    image: "/assets/logos/vacations1.png",
+    alt: "Vacations",
+  },
+  {
+    image: "/assets/logos/villa1.png",
+    alt: "Villa",
+  },
 ];
 
 const Carousel: React.FC = () => {
   return (
     <div className="flex justify-around items-center p-4">
       {logos.map((logo, index) => (
-        <img key={index} src={logo} alt="Logo" className="w-6 cursor-pointer"/>
+        <img
+          key={index}
+          src={logo.image}
+          alt={logo.alt}
+          className="w-6 cursor-pointer"
+        />
       ))}
     </div>
   );
