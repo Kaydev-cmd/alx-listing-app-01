94ca97daa55c487e68ff15e65a8236f43ea5aa15
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-12 07:50:11 +0200

    feat: render property name, star rating, and city/country in PropertyDetail

--- components/property/PropertyDetail.tsx
+++ components/property/PropertyDetail.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { PropertyProps } from "@/interfaces";
 
-const PropertyDetail: React.FC = () => {
-  return <div>PropertyDetail</div>;
+const PropertyDetail: React.FC<{ property: PropertyProps }> = ({
+  property,
+}) => {
+  return (
+    <div className="container mx-auto p-6">
+      <h1 className="text-4xl font-bold">{property.name}</h1>
+      <div className="flex items-center space-x-2 mt-2">
+        <span className="text-yellow-500">{property.rating} stars</span>
+        <span>
+          {property.address.city}, {property.address.country}
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default PropertyDetail;
