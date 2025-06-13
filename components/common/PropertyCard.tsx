f79491aedd254119564446d570bc088df134fcab
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-13 09:09:36 +0200

    refactor: reduce icon size

--- components/common/PropertyCard.tsx
+++ components/common/PropertyCard.tsx
@@ -18,11 +18,11 @@ const PropertyCard: React.FC<PropertyCardProps> = ({
   return (
     <div className="bg-white rounded-xl shadow-md overflow-hidden w-[600px] max-w-sm transition-transform hover:scale-105 cursor-pointer">
       <img src={image} alt={name} className="w-full h-48 object-cover" />
-      <ul className="flex flex-wrap space-x-4 mt-2 ml-2">
+      <ul className="flex flex-wrap space-x-4 mt-4 ml-3">
         {amenities.map((amenity, index) => (
           <li
             key={index}
-            className="bg-gray-200 rounded-md py-1 px-2 text-sm font-semibold"
+            className="bg-[#F9F9F9] rounded-full py-1 px-2 text-sm font-semibold"
           >
             {amenity}
           </li>
@@ -39,17 +39,17 @@ const PropertyCard: React.FC<PropertyCardProps> = ({
           {address.city}, {address.country}
         </h2>
         <div className="flex justify-between flex-row-reverse items-center">
-          <p className="text-[#34967C] font-medium mb-1 text-lg">{price}/n</p>
+          <p className="text-[#161117] font-medium mb-1 text-lg">{price}/n</p>
           <div className="flex justify-center gap-2 bg-white border border-[#e9e9e9] rounded-full py-2 text-sm font-semibold w-[160px]">
-            <p className="flex items-center gap-1">
-              <IoIosBed size={22} /> {offers.bed}
+            <p className="flex items-center gap-2">
+              <IoIosBed size={16} /> {offers.bed}
             </p>
-            <p className="flex items-center gap-1">
-              <FaShower size={22} />
+            <p className="flex items-center gap-2">
+              <FaShower size={16} />
               {offers.shower}
             </p>
-            <p className="flex items-center gap-1">
-              <PiUsersFill size={22} />
+            <p className="flex items-center gap-2">
+              <PiUsersFill size={16} />
               {offers.occupants}
             </p>
           </div>
