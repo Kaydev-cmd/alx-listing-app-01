b48efc92d4ae6b4880c614ca3dbc6d721180ad9f
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-25 12:28:12 +0200

    feat: add responsive grid system

--- pages/booking/index.tsx
+++ pages/booking/index.tsx
@@ -7,7 +7,7 @@ import CancellationPolicy from "@/components/booking/CancellationPolicy";
 const BookingPage = () => {
   return (
     <div className="container mx-auto p-6">
-      <div className="grid grid-cols-2 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6">
         <BookingForm />
         <OrderSummary {...BOOKING_DETAILS} />
         <CancellationPolicy />
